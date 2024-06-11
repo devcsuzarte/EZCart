bde354824ad91dd2e4cce5dcbeecd3f235023709
Claudio Jr <85254941+devcsuzarte@users.noreply.github.com>
2024-06-11 05:32:47 -0300

    first commit

---
+++ EZCart/Controller/ViewController.swift
@@ -0,0 +1,135 @@
+//
+//  ViewController.swift
+//  EZCart
+//
+//  Created by Claudio Suzarte on 03/06/24.
+//
+
+import UIKit
+import Vision
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    var cartList: [Product] = []
+    var wordsGeted:[String] = []
+    var possiblePrice:[String] = []
+    var possibleProducts:[String] = []
+
+    @IBOutlet weak var titleTextLable: UILabel!
+    @IBOutlet weak var priceTextLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+    
+    
+    @IBAction func refreshTitle(_ sender: Any) {
+        titleTextLable.text = possibleProducts.randomElement()
+    }
+    
+    @IBAction func refreshPrice(_ sender: Any) {
+        priceTextLabel.text = possiblePrice.randomElement()
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userPickerImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            recognizeLabel(image: userPickerImage)
+            imagePicker.dismiss(animated: true)
+        }
+    }
+    
+    func recognizeLabel(image: UIImage?){
+        // Get the CGImage on which to perform requests.
+        guard let cgImage = image?.cgImage else { return }
+
+
+        // Create a new image-request handler.
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage)
+
+
+        // Create a new request to recognize text.
+        let request = VNRecognizeTextRequest { request, error in
+            
+            guard let observations = request.results as? [VNRecognizedTextObservation],
+                  error == nil else {
+                return
+            }
+            
+            
+           let text = observations.compactMap({
+                $0.topCandidates(1).first?.string
+            }).joined(separator: ";")
+            
+            
+            DispatchQueue.main.async {
+               // print("Sentence: \(text)")
+                self.textHandle(text: text)
+                
+            }
+        }
+
+
+        do {
+            // Perform the text-recognition request.
+            try requestHandler.perform([request])
+        } catch {
+            print("Unable to perform the requests: \(error).")
+        }
+    }
+    
+    func textHandle(text : String){
+        possiblePrice = []
+        possibleProducts = []
+        
+        wordsGeted = text.components(separatedBy: ";")
+        
+        for word in wordsGeted {
+            if word.contains(",") && word.count < 8{
+                //print("PREÇO: \(word)")
+                possiblePrice.append(word)
+            } else {
+                analiseTitle(titleCheck: word)
+            }
+        }
+        
+       // print("PREÇO: \(possiblePrice)")
+        //print("PRODUT: \(possibleProducts)")
+       titleTextLable.text = possibleProducts.randomElement()
+        priceTextLabel.text = possiblePrice.randomElement()
+
+    }
+    
+    func checkNumeric(S: String) -> Bool {
+       return Double(S) != nil
+    }
+    
+    func analiseTitle(titleCheck: String){
+        let model = try! LabelClassifier()
+        let input = LabelClassifierInput(text: titleCheck)
+        guard let output = try? model.prediction(text: titleCheck) else {
+            return
+        }
+        
+        if output.label == "title" && titleCheck.count > 5{
+            print(titleCheck)
+            possibleProducts.append(titleCheck)
+        }
+    }
+    
+    @IBAction func addToCartButtonPressed(_ sender: UIButton) {
+        var newProduct = Product()
+        newProduct.label = titleTextLable.text
+        newProduct.priceLabel = priceTextLabel.text
+        print(newProduct)
+    }
+    
+    @IBAction func scanButtonPressed(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+}
+
---
+++ EZCart/Model/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  EZCart
+//
+//  Created by Claudio Suzarte on 11/06/24.
+//
+
+import Foundation
+
+struct Product {
+    
+    var label: String?
+    var priceLabel: String?
+  //var priceReal: Float
+  //var amount: Int
+}
